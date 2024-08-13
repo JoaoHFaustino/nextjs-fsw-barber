c5c2a970c34c23e15979426a8e5216f6c7280195
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-08-13 13:05:14 -0300

    feat: add header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button>Test</Button>
+  return (
+    <div className="p-5">
+      <Header />
+      <h2 className="text-xl font-bold">Olá, João!</h2>
+      <p>Segunda-feira, 06 de agosto</p>
+
+      <div className="mt-6 flex items-center gap-2">
+        <Input placeholder="Faça sua busca..."></Input>
+        <Button>
+          <SearchIcon></SearchIcon>
+        </Button>
+      </div>
+
+      <div className="relative mt-6 h-[150px] w-full">
+        <Image
+          alt="Agende nos melhores com FSW Barber"
+          src={"/banner-01.png"}
+          fill
+          className="rounded-xl object-cover"
+        />
+      </div>
+    </div>
+  )
 }
