515a409b2f04e732590484aa3d86533b1564a7b6
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-08-13 18:14:25 -0300

    feat: add BookingItem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
+
 import { db } from "./_lib/prisma"
 import BarberShopItem from "./_components/babershop-item"
 import { quickSearchOptions } from "./_constants/quickSearch"
+import BookingItem from "./_components/booking-item"
 
 export default async function Home() {
   const barberShops = await db.barbershop.findMany()
@@ -58,31 +58,7 @@ export default async function Home() {
         </div>
 
         {/* Agendamento */}
-        <h2 className="mb-3 mt-6 font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* Esquerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-              <div className="gap2 flex items-center">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
-                </Avatar>
-                <p className="text-sm">Barbearia FWS</p>
-              </div>
-            </div>
-            {/* Direita */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">13</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
-
+        <BookingItem></BookingItem>
         {/* Recomendados */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
